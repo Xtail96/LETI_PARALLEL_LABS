014b20bf1d74948efbc264c11ca20ec026f90902
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2019-12-24 09:39:03 +0300

    lab4 intial

--- assembly.sh
+++ assembly.sh
@@ -9,3 +9,6 @@ case ${1} in
 		mpicc -o ${2}.out ${2}.c
 		mpirun --hostfile hostfile -np ${3} ${2}.out;;
 esac
+
+#mpic++ -std=c++11 lab4.cpp  -lboost_mpi -lboost_serialization
+#mpic++ -std=c++11 -o lab4.out  lab4.cpp
