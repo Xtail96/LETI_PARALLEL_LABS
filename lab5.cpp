6c5e2b77a117f3c56cf96b8ded3a6e78ea2a2df1
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2019-12-24 23:12:15 +0300

    upd

--- lab5.cpp
+++ lab5.cpp
@@ -1,10 +1,7 @@
 #include <mpi.h>
-//#include <stdafx.h>
 #include <stdio.h>
 #include <stdlib.h>
-//#include <conio.h>
-//#include <curses.h>
-//
+
 #include <math.h>
 #include <ctime>
 #include <chrono>
@@ -57,63 +54,7 @@ void Flip (double *&B, int dim) {
     }
 }
 //-------------------------------------------------
-void MatrixMultiplicationMPI(double *&A, double *&B, double *&C, int &Size) {
-    int dim =Size;
-    int i, j, k, p, ind;
-    double temp;
-    MPI_Status Status;
-    int ProcPartSize=dim/ProcNum;
-    int ProcPartElem=ProcPartSize*dim;
-    double* bufA=new double[dim*ProcPartSize];
-    double* bufB=new double[dim*ProcPartSize];
-    double* bufC=new double[dim*ProcPartSize];
-    int ProcPart = dim/ProcNum, part = ProcPart*dim;
-    if (ProcRank == 0) {
-        Flip(B,Size);
-    }
-    
-    MPI_Scatter(A, part, MPI_DOUBLE, bufA, part, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Scatter(B, part, MPI_DOUBLE, bufB, part, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    
-    temp=0.0;
-    for(i=0;i<ProcPartSize;i++) {
-        for(j=0;j<ProcPartSize; j++) {
-            for(k=0;k<dim;k++) 
-                temp+=bufA[i*dim+k]*bufB[j*dim+k];
-            bufC[i*dim+j+ProcPartSize*ProcRank]=temp;
-            temp=0.0;
-        }
-    }
-    
-    int NextProc; int PrevProc;
-    for(p=1; p<ProcNum; p++) {
-        NextProc = ProcRank+1;
-        if ( ProcRank ==ProcNum-1 ) NextProc = 0;
-        PrevProc = ProcRank - 1;
-        if ( ProcRank == 0 ) PrevProc = ProcNum-1;
-        MPI_Sendrecv_replace(bufB,part, MPI_DOUBLE, NextProc, 0, PrevProc, 0, MPI_COMM_WORLD, &Status);
-        temp=0.0;
-        for (i=0; i<ProcPartSize; i++) {
-            for(j=0; j<ProcPartSize; j++) {
-                for(k=0; k<dim; k++){
-                    temp +=bufA[i*dim+k]*bufB[j*dim+k];}
-                if (ProcRank - p >= 0 )
-                    ind=ProcRank - p;
-                else ind=(ProcNum - p+ProcRank);
-                bufC[i*dim+j+ind*ProcPartSize]=temp;
-                temp=0.0;
-            }
-        }
-    }
-    MPI_Gather(bufC, ProcPartElem, MPI_DOUBLE, C, ProcPartElem, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    
-    delete []bufA;
-    delete []bufB;
-    delete []bufC;
-}
-
 void MatrixMultiplicationMPI2(double *A, double *B,  double *C,int size){
-
    int i = 0, j = 0;
 
    double *bufA, *bufB, *bufC;
@@ -122,79 +63,50 @@ void MatrixMultiplicationMPI2(double *A, double *B,  double *C,int size){
    MPI_Status Status;
 
    int ProcPartsize = dim/ProcNum;
-
    int ProcPartElem = ProcPartsize*dim;
 
    bufA = new double[ProcPartElem];
-
    bufB = new double[ProcPartElem];
-
    bufC = new double[ProcPartElem];
 
    for (i = 0; i < ProcPartElem; i++)
-
-      bufC[i] = 0;
-
-   MPI_Scatter(A, ProcPartElem, MPI_DOUBLE, bufA, ProcPartElem, MPI_DOUBLE, 
-
-               0, MPI_COMM_WORLD);
-
-   MPI_Scatter(B, ProcPartElem, MPI_DOUBLE, bufB, ProcPartElem, MPI_DOUBLE,
-
-               0, MPI_COMM_WORLD);
+   {
+        bufC[i] = 0;
+   }
+      
+   MPI_Scatter(A, ProcPartElem, MPI_DOUBLE, bufA, ProcPartElem, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+   MPI_Scatter(B, ProcPartElem, MPI_DOUBLE, bufB, ProcPartElem, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
    int k = 0 ;
-
- 
-
    int NextProc = ProcRank + 1;
-
    if ( ProcRank == ProcNum - 1 ) NextProc = 0;
 
    int PrevProc = ProcRank - 1;
-
    if ( ProcRank == 0 ) PrevProc = ProcNum - 1;
 
- 
-
    int PrevDataNum = ProcRank;
-
- 
-
    for (int p = 0; p < ProcNum; p++)
-
    {
-
-      for (i = 0; i < ProcPartsize; i++)
-
-         for (j = 0; j < size; j++)
-
-         {
-
-            double tmp = 0;
-
-                  for (k = 0; k < ProcPartsize; k++)
-
-                        tmp += bufA[PrevDataNum * ProcPartsize + i * size + k] * bufB[k * size + j];
-
-              bufC[i * size + j] += tmp;
-
+        for (i = 0; i < ProcPartsize; i++)
+        {
+            for (j = 0; j < size; j++)
+            {
+                double tmp = 0;
+                for (k = 0; k < ProcPartsize; k++)
+                    tmp += bufA[PrevDataNum * ProcPartsize + i * size + k] * bufB[k * size + j];
+                bufC[i * size + j] += tmp;
             }
+        }
 
-     
+        PrevDataNum -= 1;
 
-      PrevDataNum -= 1;
+        if (PrevDataNum < 0)
+            PrevDataNum = ProcNum - 1;
 
-      if (PrevDataNum < 0)
-
-              PrevDataNum = ProcNum - 1;
-
-      MPI_Sendrecv_replace(bufB, ProcPartElem, MPI_DOUBLE, NextProc, 0, PrevProc, 0, MPI_COMM_WORLD, &Status);
-
+        MPI_Sendrecv_replace(bufB, ProcPartElem, MPI_DOUBLE, NextProc, 0, PrevProc, 0, MPI_COMM_WORLD, &Status);
    }
 
    MPI_Gather(bufC, ProcPartElem, MPI_DOUBLE, C, ProcPartElem, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
 }
 
 //--------------------------------------------------------
@@ -204,12 +116,11 @@ int main(int argc, char **argv) {
     MPI_Init ( &argc, &argv );  
     InitProcess (A,B,C,Size);
     beg=MPI_Wtime();
-    MatrixMultiplicationMPI(A,B,C,Size);
+    MatrixMultiplicationMPI2(A,B,C,Size);
     end=MPI_Wtime(); 
     parallel= end-beg; 
 
     if (ProcRank == 0) {
-        // printf("\n",&ProcNum);
         printf ("\n");
         printf("\nTime of execution -  Parallel calculation:\n");
         printf("%7.4f",parallel);
@@ -227,16 +138,18 @@ int main(int argc, char **argv) {
         
         auto non_paral_end=std::chrono::system_clock::now(); 
         notparallel= std::chrono::duration<double>(end-beg).count(); 
-        printf("\nTime of execution -  Not Parallel calculation:\n");
+        printf("\nTime of execution -  Sequential calculation:\n");
         printf("%7.4f",notparallel);
         
         if (flag) {
             printf("\nMatrix C - Parallel calculation\n");
             PrintMatrix(C,Size);
+            printf("\n\n");
         }   
     }
     MPI_Finalize();
-    delete [] A; delete [] B; delete [] C;  
+    delete [] A;
+    delete [] B;
+    delete [] C;  
     // getch();
-    
 }
