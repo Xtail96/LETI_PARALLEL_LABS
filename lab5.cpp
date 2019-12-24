f92ae386cdee0ba476c7df1a2066abe90f8b6ea7
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2019-12-25 01:02:10 +0300

    upd

--- lab5.cpp
+++ lab5.cpp
@@ -54,7 +54,8 @@ void Flip (double *&B, int dim) {
     }
 }
 //-------------------------------------------------
-void MatrixMultiplicationMPI2(double *A, double *B,  double *C,int size){
+double MatrixMultiplicationMPI2(double *A, double *B,  double *C,int size){
+	double beg, end, serial, parallel;
    int i = 0, j = 0;
 
    double *bufA, *bufB, *bufC;
@@ -74,6 +75,7 @@ void MatrixMultiplicationMPI2(double *A, double *B,  double *C,int size){
         bufC[i] = 0;
    }
       
+    beg=MPI_Wtime();
    MPI_Scatter(A, ProcPartElem, MPI_DOUBLE, bufA, ProcPartElem, MPI_DOUBLE, 0, MPI_COMM_WORLD);
    MPI_Scatter(B, ProcPartElem, MPI_DOUBLE, bufB, ProcPartElem, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
@@ -107,9 +109,15 @@ void MatrixMultiplicationMPI2(double *A, double *B,  double *C,int size){
    }
 
    MPI_Gather(bufC, ProcPartElem, MPI_DOUBLE, C, ProcPartElem, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+   
+    end=MPI_Wtime(); 
+    parallel= end-beg;
+    return parallel;
 }
 
-void MatrixMultiplicationSequential(double *A, double *B, double *C, int size) {
+double MatrixMultiplicationSequential(double *A, double *B, double *C, int size) {
+    //auto non_paral_beg=std::chrono::system_clock::now();
+    double sync_begin = MPI_Wtime();
     for (int i=0; i<Size; i++){
         for (int j=0; j<Size;j++) {
             C[i*Size+j]=0;
@@ -118,28 +126,28 @@ void MatrixMultiplicationSequential(double *A, double *B, double *C, int size) {
             }
         }
      }
+    double sync_end = MPI_Wtime();
+    return sync_end - sync_begin;
+    //auto non_paral_end=std::chrono::system_clock::now();
+    //return std::chrono::duration<double>(non_paral_end-non_paral_beg).count();
 }
 
 int main(int argc, char **argv) {
-    double beg, end, serial, parallel, notparallel=0;
+    double beg, end, serial;
+    double async_latency;
+    double sync_latency;
     MPI_Init ( &argc, &argv );  
     InitProcess (A,B,C,Size);
-    beg=MPI_Wtime();
-    MatrixMultiplicationMPI2(A,B,C,Size);
-    end=MPI_Wtime(); 
-    parallel= end-beg; 
+    async_latency = MatrixMultiplicationMPI2(A,B,C,Size);
 
     if (ProcRank == 0) {
         printf ("\n");
         printf("\nTime of execution -  Parallel calculation:\n");
-        printf("%7.4f",parallel);
-        
-        auto non_paral_beg=std::chrono::system_clock::now();;
-        MatrixMultiplicationSequential(A, B, C, Size);
-        auto non_paral_end=std::chrono::system_clock::now();; 
-        notparallel=std::chrono::duration<double>(non_paral_end-non_paral_beg).count();; 
+        printf("%7.4f",async_latency);
+         
+        sync_latency= MatrixMultiplicationSequential(A, B, C, Size); 
         printf("\nTime of execution -  Sequential calculation:\n");
-        printf("%7.4f",notparallel);
+        printf("%7.4f",sync_latency);
         
         if (flag) {
             printf("\nMatrix C - Parallel calculation\n");
